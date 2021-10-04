39b3e921eed020dec45c3219b6ded6eb9dc0b337
yashchellani <yashcchellani@gmail.com>
2021-10-04 15:32:10 +0800

    registration page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Login from "./components/Login/Login.jsx"
+import Register from "./components/Registration/Registration"
 
 function App() {
   return (
     <Router>
     <Switch>
-        <Route path='/' exact component={Login}/>
+        <Route path='/' exact component={Register}/>
     </Switch>
     </Router>
   );
