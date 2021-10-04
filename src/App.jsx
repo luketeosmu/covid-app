83b0648f561bdf43d4a0abd14f669342658727ea
yashchellani <yashcchellani@gmail.com>
2021-10-04 16:05:35 +0800

    added employees

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Login from "./components/Login/Login.jsx"
 import NavBar from './components/NavBar';
+import EmployeeList from './components/Employees/EmployeeList.jsx';
 
 function App() {
   return (
     <Router>
     <Switch>
         <Route path='/' exact component={Register}/>
+        <Route path='/Employees' exact component={EmployeeList}/>
+
     </Switch>
     </Router>
   );
