18cb533833b8927cb04806f21d4a4cb92633a159
ststheting <shaunts5111@gmail.com>
2021-10-05 17:25:21 +0800

    add employee added

--- src/components/Employees/EmployeeList/EmployeeList.jsx
+++ src/components/Employees/EmployeeList/EmployeeList.jsx
@@ -1,23 +1,41 @@
 import React from "react";
-import { Table, Card, InputGroup, Form, Button, Container } from "react-bootstrap";
+import { Table, Card, InputGroup, Form, Button, Container, Tooltip, OverlayTrigger} from "react-bootstrap";
 import "./EmployeeList.css";
 import NavBar from "../../NavBar/NavBar";
 import { useState } from 'react'
 
-function EmployeeList() {
+const EmployeeList = () => {
     const employees = [
-        { id: 42069, name: "Otto", status: true },
-        { id: 12345, name: "John", status: false },
-        { id: 12089, name: "Mary", status: true },
-        { id: 12459, name: "Steve", status: false },
+        { id: 12, name: "Otto", status: true },
+        { id: 34, name: "John", status: false },
+        { id: 45, name: "Mary", status: true },
+        { id: 56, name: "Steve", status: false },
+        { id: 78, name: "Kang", status: false },
+        { id: 90, name: "Roys", status: false },
+        { id: 321, name: "Jian Yi", status: true },
+        { id: 324, name: "Yash", status: false },
+        { id: 656, name: "Luke", status: true },
+        { id: 767, name: "Shaun", status: true },
+        { id: 986, name: "Sean", status: true },
+        { id: 4213, name: "Josh", status: true },
+        { id: 5739, name: "Rahmat", status: true },
+        { id: 7683, name: "Otto", status: false },
+        { id: 9778, name: "Jennifer", status: true },
+        { id: 12487, name: "William", status: true },
     ]
 
     const [searchTerm, setSearchTerm] = useState("")
 
+    const renderTooltip = (props) => (
+        <Tooltip className="me-2"id="button-tooltip" {...props}>
+            Add New Employee
+        </Tooltip>
+    );
+
     return (
         <div>
             <NavBar />
-            <Card className="text-center">
+            <Card className="text-center pb-5">
                 <Card.Header as="h2">Manage Employees</Card.Header>
                 <div className="d-flex justify-content-center mt-5">
                     <h4> List of Employees </h4>
@@ -26,8 +44,8 @@ function EmployeeList() {
                     <InputGroup>
                         <Form.Control
                             type="text"
-                            placeholder="Search/Filter by Employee ID, Employee Name, Vaccinated/Unvaccinated..."
-                            onChange = {(e) => {
+                            placeholder="Try searching by Employee ID, Employee Name, Vaccinated/Unvaccinated..."
+                            onChange={(e) => {
                                 setSearchTerm(e.target.value)
                             }}
                         />
@@ -38,7 +56,7 @@ function EmployeeList() {
                         </Button>
                     </InputGroup>
                 </Container>
-                <div className="Table">
+                <div className="Table mb-5">
                     <Table striped bordered hover>
                         <thead>
                             <tr>
@@ -49,26 +67,26 @@ function EmployeeList() {
                         </thead>
                         <tbody>
                             {employees
-                            .filter((employee) => {
-                                if(searchTerm === ""){
-                                    return employee
-                                } else if (employee.name.toLowerCase().includes(searchTerm.toLowerCase())){
-                                    return employee
-                                } else if (employee.id.toString().includes(searchTerm.toString())){
-                                    return employee
-                                } else if (employee.status == true && (searchTerm.toLowerCase() == "yes" || searchTerm.toLowerCase() == "vaccinated")){
-                                    return employee
-                                } else if (employee.status == false && (searchTerm.toLowerCase() == "no" || searchTerm.toLowerCase() == "unvaccinated")) {
-                                    return employee
-                                }
-                            })
-                            .map(employee => (
-                                <tr key={employee.id}>
-                                    <td>{employee.id}</td>
-                                    <td>{employee.name}</td>
-                                    <td>{employee.status ? "Yes" : "No"}</td>
-                                </tr>
-                            ))}
+                                .filter((employee) => {
+                                    if (searchTerm === "") {
+                                        return employee
+                                    } else if (employee.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                                        return employee
+                                    } else if (employee.id.toString().includes(searchTerm.toString())) {
+                                        return employee
+                                    } else if (employee.status == true && (searchTerm.toLowerCase() == "yes" || searchTerm.toLowerCase() == "vaccinated")) {
+                                        return employee
+                                    } else if (employee.status == false && (searchTerm.toLowerCase() == "no" || searchTerm.toLowerCase() == "unvaccinated")) {
+                                        return employee
+                                    }
+                                })
+                                .map(employee => (
+                                    <tr key={employee.id}>
+                                        <td>{employee.id}</td>
+                                        <td>{employee.name}</td>
+                                        <td>{employee.status ? "Yes" : "No"}</td>
+                                    </tr>
+                                ))}
                             {/* <tr>
                                 <td>69420</td>
                                 <td>Otto</td>
@@ -77,6 +95,26 @@ function EmployeeList() {
                         </tbody>
                     </Table>
                 </div>
+                <Container className="mb-5 fixed-bottom text-end">
+                    <OverlayTrigger
+                        placement="left"
+                        delay={{ show: 250, hide: 250 }}
+                        overlay={renderTooltip}
+                    >
+                        <Button style={{
+                            width: "60px",
+                            height: "60px",
+                            padding: "10px 16px",
+                            borderRadius: "35px",
+                            fontSize: "12px",
+                            textAlign: "center"
+                        }}
+                            className="mt-5"
+                            variant="primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+                            </svg></Button>
+                    </OverlayTrigger>
+                </Container>
             </Card>
         </div>
     );
