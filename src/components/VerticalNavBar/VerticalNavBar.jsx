1e93e042ef5314baa929d8e31bd1d02d3bf82ffd
cskang0121 <kang.chinshen@gmail.com>
2021-10-06 14:18:50 +0800

    vertical-navbar

--- src/components/VerticalNavBar/VerticalNavBar.jsx
+++ src/components/VerticalNavBar/VerticalNavBar.jsx
@@ -1,6 +1,27 @@
+import React from 'react'
+import { Nav, Container } from 'react-bootstrap'
+
 const VerticalNavBar = () => {
     return ( 
-        <h1>Vertical NavBar</h1>
+        <div class="verticalNavBar">
+            <Nav className="flex-column mt-3">
+                <Nav.Item>
+                    <Nav.Link href="#" style={{color: "white"}}>INDOORS</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="#" style={{color: "white"}}>OUTDOORS</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="#" style={{color: "white"}}>OTHERS...</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="#" style={{color: "white"}}>OTHERS...</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="#" style={{color: "white"}}>OTHERS...</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </div>
     );
 }
  
