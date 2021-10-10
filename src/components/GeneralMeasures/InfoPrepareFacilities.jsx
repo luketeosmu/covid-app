05f6dfce12c93369aed690df250ba307b252bc1d
cskang0121 <kang.chinshen@gmail.com>
2021-10-10 22:20:44 +0800

    InfoPrepareFacilities

--- src/components/GeneralMeasures/InfoPrepareFacilities.jsx
+++ src/components/GeneralMeasures/InfoPrepareFacilities.jsx
@@ -1,7 +1,162 @@
+import './Info.css'
+
 const InfoPrepareFacilities = () => {
     return (
-        <div id="InfoPrepareFacilities">
-            <h2>kang</h2>
+        <div>
+            <h1 id="InfoPrepareFacilities" className="title-level1">Preparing Facilities</h1>
+            <h2 id="InfoAttractionsSetUp" className="title-level2">Attractions Set Up</h2>
+                <h3 className="title-level3">General</h3>
+                    <p>
+                    <span> 1. Develop and implement detailed physical layout plans, including reconfiguration of spaces if applicable, at entry and exit points to achieve safe circulation with at least 1m distancing between visitors who are not from the same group, ensuring groups do not have more than 5 pax, and with at least 1m separation between groups throughout the attraction.<br/></span>
+
+                    <span> 2. Clearly mark out queue lines with at least 1m safe distancing where queues are likely to form, including at F&B and retail outlets.<br/></span>
+
+                    <span>3. Develop and implement signs reminding visitors to practice safe distancing and all other SMMs applicable to visitors (e.g. mask wearing, stay in groups of not more than 5 pax).<br/></span>
+
+                    <span>4. Close off components/ areas within the attraction where SMMs cannot be implemented.<br/></span>
+
+                    <span>5. Implement contactless payment for all payment functions within the attraction (e.g. ticketing, F&B and retail outlets).<br/></span>
+
+                    <span>6. Install hygiene screens at human touch points (e.g. glass or plastic dividers/barriers at ticketing booths separating the ticket staff from visitors), if reasonably practicable.<br/></span>
+
+                    <span>7. Set up one-way traffic flow for visitors to enter and exit the attraction, if reasonably practicable.<br/></span>
+                    
+                    <span>8. For resumption of dining-in at F&B and indoor high-intensity activities where masks are not worn or where there is provision of common equipment, set-up and check the status of visitors as to whether they are fully vaccinated1 . Otherwise, F&B will only be allowed to operate take-away or delivery services, and indoor high-intensity activities where masks are not worn will not be allowed.<br/></span>
+                    </p>
+                <h3 className="title-level3">Rides, Shows and Tours</h3>
+                    <p>
+                    <span>Develop and implement detailed plans for attendance management, crowd control and cleaning frequency for rides and shows:<br/></span>
+
+                    <span className="point">
+                    • Seating plan for rides and all seating within the attraction that are not within shows or live performances. Where seating is provided that is not fixed to the floor, ensure seats are spaced at least 1m apart. Where seating is provided that is fixed to the floor, demarcate alternate seats or seating spaces that should not be occupied unless the visitor is from the same group as those in the adjacent seats). 
+                    </span>
+
+                    <span className="point">
+                    • Seating plan for shows and live performances. Ensure groups are seated at least 1m apart.
+                    </span>
+
+                    <span className="point">
+                    • Entry queue management system with markers placed at least 1m apart throughout the attraction.
+                    </span>
+
+                    <span className="point">
+                    • Cleaning protocols for rides and show equipment and surfaces.
+                    </span>
+                    </p>
+
+                    <h4 className="title-level4">Indoor Shows</h4>
+                        <p>
+                        <span className="point">
+                        • Adhere to IMDA’s prevailing advisory for Cinemas for indoor screenings.
+                        </span>
+
+                        <span className="point">
+                        • The maximum capacity for indoor shows is up to 1,000 pax in zones of up to 50 pax per zone if all individuals are fully vaccinated, or 50 pax without PET, or the safe venue capacity with SMMs in place, whichever is lower.Ensure at least 1m safe distancing between groups. 
+                        </span>
+
+                        <span className="point">
+                        • F&B may be served in groups of up to 5 pax if all customers are vaccinated.
+                        </span>
+                        </p>
+
+                    <h4 className="title-level4">Outdoor Shows</h4>
+                        <p>
+                        <span>Capacity and Audience Separation<br/></span>
+                        <span className="point">
+                        • The maximum capacity for outdoor shows is up to 1,000 pax in zones of up to 50 pax per zone if all individuals are fully vaccinated, or 50 pax without PET, or the safe venue capacity with SMMs in place, whichever is lower. Ensure at least 1m safe distancing between groups.
+                        </span>
+
+                        <span className="point">
+                        • There must be at least 3m separation between the stage/performers and audience.
+                        </span>
+
+                        <span className="point">
+                        • Zones must be clearly demarcated and there must be a separation of at least 3m between zones, demarcated by continuous physical barriers (e.g. different levels, partitions, barrier ropes or tapes). Ensure no mingling between zones.
+                        </span>
+
+                        <span className="point">
+                        • Implement SafeEntry check-in for each zone at the show and ensure that the TraceTogether mobile application and TraceTogether Token can be used for SafeEntry check-in.
+                        </span>
+                        </p>
+
+                    <h4 className="title-level4">Live Performances</h4>
+                        <p>
+                        <span className="point">
+                        • Adhere to NAC’s prevailing advisory for Live Performances, and any additional requirements imposed by MTI.
+                        </span>
+                        </p>
+
+                    <h4 className="title-level4">Tours</h4>
+                        <p>
+                        <span className="point">
+                        • Adhere to STB’s prevailing circular for Tours5 where relevant, and any additional requirements imposed by MTI.
+                        </span>
+
+                        <span className="point">
+                        • The maximum number of participants for conveyance tour is up to 50, with groups of no more than 5 pax within each such tour. The maximum number of participants for a nonconveyance tour remains at 20, with groups of no more than 5 pax within each such tour.
+                        </span>
+                        </p>
+
+                <h3 className="title-level3">Work-Related Events</h3>
+                    <p>
+                    <span>1. Some work-related events have been allowed to resume at workplace premises and third-party venues.<br/></span>
+
+                    <span>2. For non consumer-facing events:<br/></span>
+
+                    <span className="point">
+                    • Such events organised by attractions are allowed to resume within the attractions’ workplace premises. 
+                    </span>
+
+                    <span className="point">
+                    • Attractions can be hired as a function centre for the purpose of the event and attractions can accept corporate bookings from external parties for such work-related events.
+                    </span>
+
+                    <span className="point">
+                    • Attraction are to ensure the event has a maximum capacity of 50 pax or the safe venue capacity with SMMs in place, whichever is lower. Such events exceeding 50 participants will not be allowed in attractions.
+                    </span>
+
+                    <span className="point">
+                    • Ensure at least 1m spacing between individuals at all times.
+                    </span>
+
+                    <span className="point">
+                    • Adhere to ESG’s prevailing advisory for F&B8 where F&B is served and consumed at the event.
+                    </span>
+
+                    <span>3. For consumer-facing events:<br/></span>
+
+                    <span className="point">
+                    • Such events organised by attractions within their own premises are subject to the prevailing operating capacity that attractions are allowed to operate at or the safe venue capacity with SMMs in place, whichever is lower.
+                    </span>
+
+                    <span className="point">
+                    • Events organised by external parties (where the attraction now function as a third party venue) are subject to a cap of 50 persons (excluding the establishment’s service staff) or the safe venue capacity with SMMs in place, whichever is lower.
+                    </span>
+
+                    <span className="point">
+                    • Ensure at least 1m spacing at all times between individuals and between groups of not more than 5 pax.
+                    </span>
+
+                    <span className="point">
+                    • Adhere to ESG’s prevailing advisory for F&B where F&B is served and consumed at the event.
+                    </span>
+                    </p>
+
+            <h2 id="InfoOperatingCapacity" className="title-level2">Operating Capacity</h2>
+            <p>
+            <span> 1. Operate at no more than 50% of total capacity at any one time, until otherwise approved by MTI, and show how the maximum capacity of attraction is derived.<br/></span>
+
+            <span> 2. Implement solutions to monitor, control and enforce the operating capacity (e.g. manual counting, automated people counting or tracking systems).<br/></span>
+            </p>
+
+            <h2 id="InfoMngProxTouchedSurf" className="title-level2">Management of Proximity and Touched Surfaces</h2>
+            <p>
+            <span> 1. Ensure that physical interaction between visitors and staff (including volunteers) is minimised where reasonably practicable.<br/></span>
+
+            <span> 2. Eliminate or else minimise sharing of equipment and tools.<br/></span>
+
+            <span> 3. Provide easily accessible and free-to-use disinfecting agents like and sanitisers, disinfecting sprays and wipes at high-touch areas.<br/></span>
+            </p>
         </div>
     );
 }
